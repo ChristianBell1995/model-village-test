5335314bfd61159dda5e2f7a30a9eb0576d775f5
Christian Bell <christiantcbell@gmail.com>
2018-09-17 17:36:54 +0100

    made a couple of minor adjustments to the css and began to think about the JS for the dropdown menu

---
+++ app/javascript/index.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("DOM fully loaded and parsed");
+//     var dropDownMenuText = document.getElementById("drop-down-menu-text");
+//     console.log(dropDownMenuText.innerText);
+//     const dropDownUl = document.querySelectorAll("#drop-down-ul li");
+//     dropDownUl.addEventListener("click", (event) => {
+//       console.log(event);
+//     })
+//   })
